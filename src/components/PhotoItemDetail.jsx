b55a78642150f0bdd9abe432da6f5b354852b443
rafiandria23 <rafiandria23@gmail.com>
2020-03-05 20:58:03 +0700

    Added User Detail on PhotoItemDetail
    Used Material UI CardHeader.

--- src/components/PhotoItemDetail.jsx
+++ src/components/PhotoItemDetail.jsx
@@ -1,32 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { makeStyles } from '@material-ui/core/styles';
-import { Paper, CardMedia, IconButton, Typography } from '@material-ui/core';
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Paper,
+  CardHeader,
+  CardMedia,
+  Avatar,
+  IconButton,
+  Typography
+} from "@material-ui/core";
 import {
   Favorite as FavoriteIcon,
   GetApp as GetAppIcon,
   Comment as CommentIcon
-} from '@material-ui/icons';
+} from "@material-ui/icons";
 
 const useStyles = makeStyles({
   photoItemDetail: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    paddingRight: '3vh'
+    display: "flex",
+    justifyContent: "space-between",
+    paddingRight: "3vh"
   },
   photoDetailProperty: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center'
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center"
   },
   img: {
-    width: 'auto',
-    height: 'auto',
-    maxWidth: '720px',
-    objectFit: 'contain',
-    borderTopLeftRadius: '5px',
-    borderBottomLeftRadius: '5px'
-  }
+    width: "auto",
+    height: "auto",
+    maxWidth: "720px",
+    objectFit: "contain",
+    borderTopLeftRadius: "5px",
+    borderBottomLeftRadius: "5px"
+  },
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+    margin: 0
+  },
 });
 
 export default function PhotoItemDetail(props) {
@@ -44,36 +59,47 @@ export default function PhotoItemDetail(props) {
         <Paper elevation={5} className={classes.photoItemDetail}>
           <CardMedia
             className={classes.img}
-            component='img'
+            component="img"
             src={currentPhoto.largeImageURL}
           />
           <div>
             <div className={classes.photoDetailProperty}>
-              <IconButton size='medium'>
-                <FavoriteIcon fontSize='medium' />
+              <IconButton size="medium">
+                <FavoriteIcon />
               </IconButton>
-              <Typography variant='subtitle1'>
+              <Typography variant="subtitle1">
                 {currentPhoto.favorites} Likes
               </Typography>
             </div>
 
             <div className={classes.photoDetailProperty}>
-              <IconButton size='medium'>
-                <GetAppIcon fontSize='medium' />
+              <IconButton size="medium">
+                <GetAppIcon />
               </IconButton>
-              <Typography variant='subtitle1'>
+              <Typography variant="subtitle1">
                 {currentPhoto.downloads} Downloads
               </Typography>
             </div>
 
             <div className={classes.photoDetailProperty}>
-              <IconButton size='medium'>
-                <CommentIcon fontSize='medium' />
+              <IconButton size="medium">
+                <CommentIcon />
               </IconButton>
-              <Typography variant='subtitle1'>
+              <Typography variant="subtitle1">
                 {currentPhoto.comments} Comments
               </Typography>
             </div>
+            <CardHeader
+              className={classes.root}
+              avatar={
+                <Avatar
+                  aria-label={currentPhoto.user}
+                  src={currentPhoto.userImageURL}
+                  className={classes.avatar}
+                />
+              }
+              title={currentPhoto.user}
+            />
           </div>
         </Paper>
       )}
