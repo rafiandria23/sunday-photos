ac91ac5513ceafab96beae16a701cdd07d9a9638
rafiandria23 <rafiandria23@gmail.com>
2020-03-05 19:20:05 +0700

    - Fixed Add and Remove from Favorites.

--- src/reducers/photosReducer.js
+++ src/reducers/photosReducer.js
@@ -6,30 +6,35 @@ const initialState = {
 
 const photos = (state = initialState, action) => {
   switch (action.type) {
-    case 'FETCH_PHOTOS':
+    case "FETCH_PHOTOS":
       return { ...state, photos: action.payload.photos };
 
-    case 'ADD_PHOTO':
+    case "ADD_PHOTO":
       return {
         ...state,
         photos: state.photos.concat(action.payload.photo)
       };
 
-    case 'PHOTO_DETAIL':
+    case "PHOTO_DETAIL":
       return { ...state, currentPhoto: action.payload.photo };
 
-    case 'ADD_PHOTO_FAVORITES':
+    case "ADD_PHOTO_FAVORITES":
       return {
         ...state,
         favoritePhotos: state.favoritePhotos.concat(action.payload.photoId)
       };
 
-    case 'REMOVE_PHOTO_FAVORITES':
-      const photoIndex = state.favoritePhotos.indexOf(action.payload.photoId);
-      console.log(state.favoritePhotos.slice(1));
+    case "REMOVE_PHOTO_FAVORITES":
+      const favoritePhotoIndex = state.favoritePhotos.indexOf(action.payload.photoId);
+      if (favoritePhotoIndex === 0) {
+        return {
+          ...state,
+          favoritePhotos: state.favoritePhotos.splice(1)
+        };
+      }
       return {
         ...state,
-        favoritePhotos: state.favoritePhotos.slice(photoIndex)
+        favoritePhotos: state.favoritePhotos.splice(0, favoritePhotoIndex)
       };
 
     default:
